be9e8bb31ec0d8f027c65414857d763f4011add1
Ode Manzi <manzii.ode@gmail.com>
2019-03-12 01:30:22 +0800

    Half way done with the about us page dynamic sections

--- page-about.php
+++ page-about.php
@@ -8,42 +8,17 @@ get_template_part( 'templates/page-header' );
       <div class="slider-wrappa">
           <div class="activities-slider swiper-container">
               <div class="swiper-wrapper">
+                <?php $args = array(
+                  'post_type' => 'sead_activity',
+                  'posts_per_page' => '-1',
+                );
 
-                  <div class="swiper-slide activities-slider__activity">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/img3.jpg" alt="" class="activities-slider__activity__img">
-                      <div class="activities-slider__activity__details">
-                          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vectors/icon--light-institutions.svg" alt="" class="activities-slider__activity__icon">
-                          <h3 class="activities-slider__activity__title">Research and innovation</h3>
-                      </div>
-                  </div>
-                  <!-- slide -->
+                $sead_core_activities = new WP_Query($args);
 
-                  <div class="swiper-slide activities-slider__activity">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/img3.jpg" alt="" class="activities-slider__activity__img">
-                      <div class="activities-slider__activity__details">
-                          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vectors/icon--light-institutions.svg" alt="" class="activities-slider__activity__icon">
-                          <h3 class="activities-slider__activity__title">Research and innovation</h3>
-                      </div>
-                  </div>
-                  <!-- slide -->
+                if ( $sead_core_activities->have_posts() ) : while ( $sead_core_activities->have_posts() ) : $sead_core_activities->the_post();
+                  get_template_part( 'templates/activities-slider');
+                endwhile; endif; wp_reset_postdata(); ?>
 
-                  <div class="swiper-slide activities-slider__activity">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/img3.jpg" alt="" class="activities-slider__activity__img">
-                      <div class="activities-slider__activity__details">
-                          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vectors/icon--light-institutions.svg" alt="" class="activities-slider__activity__icon">
-                          <h3 class="activities-slider__activity__title">Research and innovation</h3>
-                      </div>
-                  </div>
-                  <!-- slide -->
-                  <div class="swiper-slide activities-slider__activity">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/img3.jpg" alt="" class="activities-slider__activity__img">
-                      <div class="activities-slider__activity__details">
-                          <img src="./images/vectors/icon--light-institutions.svg" alt="" class="activities-slider__activity__icon">
-                          <h3 class="activities-slider__activity__title">Research and innovation</h3>
-                      </div>
-                  </div>
-
-                  <!-- slide -->
               </div>
           </div><!-- activities slider -->
           <div class="slider-control-button slider-control-button--prev">
@@ -59,5 +34,31 @@ get_template_part( 'templates/page-header' );
       </div>
   </div>
 </section>
+
+
+<section class="section section-intro-video pd-top--big pd-bottom--big">
+  <div class="container">
+      <div class="columns">
+          <div class="column text">
+              <h2 class="heading--primary heading--green mg-bottom--medium"><?php the_field('about_intro_heading'); ?></h2>
+              <?php the_field('about_intro_text'); ?>
+
+          </div><!-- ./column -->
+          <div class="column text--centered">
+              <div class="video">
+                  <img src="./images/img6.jpg" alt="Video Thumbnail" class="video__thumbnail">
+
+                  <a href="<?php the_field('intro_video_url'); ?>" class="iframe-lightbox-link video__play"
+                      data-padding-bottom="56.25%" aria-hidden="true" rel="lightbox">
+                      <span class="video__play__icon">
+                          <i class="fas fa-play-circle"></i>
+                      </span>
+                  </a>
+                  <div class="video__overlay"></div><!-- video overlay -->
+              </div>
+          </div>
+      </div>
+  </div>
+        </section>
 
 <?php get_footer(); ?>
---
+++ templates/activities-slider.php
@@ -0,0 +1,15 @@
+<div class="swiper-slide activities-slider__activity">
+    <?php
+      $id_image = get_field('sead_activity_background_image');
+      $activity_background_image = wp_get_attachment_image_src( $id_image, 'full');
+    ?>
+    <img src="<?php echo $activity_background_image[0]; ?>" alt="" class="activities-slider__activity__img">
+    <div class="activities-slider__activity__details">
+        <?php
+          $id_icon = get_field('sead_activity_light_icon');
+          $activity_icon = wp_get_attachment_image_src( $id_icon, 'full');
+        ?>
+        <img src="<?php echo $activity_icon[0]; ?>" alt="" class="activities-slider__activity__icon">
+        <h3 class="activities-slider__activity__title"><?php the_field('sead_activity_title'); ?></h3>
+    </div>
+</div>
