c30ab3df86ba77b799e73d53d400b2f5b7896dc7
Ode Manzi <manzii.ode@gmail.com>
2019-11-29 11:19:27 +0800

    deleted courses page and added e-courses on media page

--- page-media.php
+++ page-media.php
@@ -176,4 +176,26 @@ get_template_part('templates/page-header');
     </div>
   </div>
 </section>
+<section class="section--padded pd-top--big pd-bottom--big">
+  <div class="container">
+    <h2 class="heading--secondary heading--green mg-bottom--big">E-Courses</h2>
+    <div class="columns is-multiline">
+      <?php $args = array(
+        'post_type'         => 'ecourse',
+        'order' => 'desc',
+        'orderby' => 'publish_date',
+        'posts_per_page' => '-1'
+      );
+      $resources = new WP_Query($args);
+
+      if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+          get_template_part('templates/e-course');
+        endwhile;
+      endif;
+      wp_reset_postdata();
+      ?>
+    </div>
+  </div>
+</section>
+<!-- gallery -->
 <?php get_footer(); ?>
