18a978f526b55dbfadd138e4449648cf7a7b610b
Ode Manzi <manzii.ode@gmail.com>
2020-04-29 20:06:31 +0800

    update pages urls and position on activities page

---
+++ page-our-activities.php
@@ -0,0 +1,62 @@
+<?php
+get_header();
+get_template_part('templates/page-header');
+?>
+
+
+<?php $args = array(
+  'post_type' => 'sead_activity',
+  'posts_per_page' => '-1',
+  'meta_key'          => 'sead_activity_order',
+  'orderby' => 'meta_value_num',
+  'order'           => 'asc',
+);
+
+
+$activities = new WP_Query($args);
+
+if ($activities->have_posts()) : while ($activities->have_posts()) : $activities->the_post();
+    get_template_part('templates/approach-activities');
+
+  endwhile;
+endif;
+wp_reset_postdata();
+?>
+
+<section class="section--padded pd-top--big pd-bottom--big">
+  <div class="container">
+    <h2 class="heading--primary heading--green mg-bottom--big">Where we work</h2>
+    <div class="columns">
+      <div class="column locations-container">
+
+        <div class="locations">
+          <?php $args = array(
+            'post_type'         => 'region',
+            'meta_key'          => 'region_order',
+            'orderby' => 'meta_value_num',
+            'order'           => 'asc',
+            'posts_per_page'    => '4',
+          );
+
+          $activities = new WP_Query($args);
+
+          if ($activities->have_posts()) : while ($activities->have_posts()) : $activities->the_post();
+              get_template_part('templates/regions');
+            endwhile;
+          endif;
+          wp_reset_postdata(); ?>
+
+        </div>
+
+      </div>
+      <div class="column">
+        <div class="details-placeholder mg-top--big">
+          <p>Hover Over Province for more information</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<?php get_footer(); ?>
---
+++ page-resources.php
@@ -0,0 +1,203 @@
+<?php
+get_header();
+get_template_part('templates/page-header');
+?>
+
+<!-- content -->
+
+<!-- training materials -->
+<section class="section training-materials bg-gray pd-top--big pd-bottom--big">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <h2 class="heading--secondary heading--green mg-bottom--big">
+          Training <br>Materials
+        </h2>
+      </div>
+      <div class="column">
+        <div class="columns">
+
+          <div class="column">
+            <h2 class="heading--tertiary mg-bottom--medium">Poultry</h2>
+            <?php $args = array(
+              'post_type'         => 'training_material',
+              'posts_per_page'    => '-1',
+              'tax_query'         => array(
+                array(
+                  'taxonomy'      => 'training_material_category',
+                  'field'         => 'slug',
+                  'terms'         => 'poultry'
+                )
+              )
+            );
+            $resources = new WP_Query($args);
+
+            if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+                get_template_part('templates/tr-material');
+              endwhile;
+            endif;
+            wp_reset_postdata(); ?>
+          </div><!-- tr material category -->
+
+          <div class="column">
+            <h2 class="heading--tertiary mg-bottom--medium">Dairy</h2>
+            <?php $args = array(
+              'post_type'         => 'training_material',
+              'posts_per_page'    => '-1',
+              'tax_query'         => array(
+                array(
+                  'taxonomy'      => 'training_material_category',
+                  'field'         => 'slug',
+                  'terms'         => 'diary'
+                )
+              )
+            );
+            $resources = new WP_Query($args);
+
+            if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+                get_template_part('templates/tr-material');
+              endwhile;
+            endif;
+            wp_reset_postdata(); ?>
+          </div><!-- tr material category -->
+
+          <div class="column">
+            <h2 class="heading--tertiary mg-bottom--medium">Potato</h2>
+            <?php $args = array(
+              'post_type'         => 'training_material',
+              'posts_per_page'    => '-1',
+              'tax_query'         => array(
+                array(
+                  'taxonomy'      => 'training_material_category',
+                  'field'         => 'slug',
+                  'terms'         => 'potato'
+                )
+              )
+            );
+            $resources = new WP_Query($args);
+
+            if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+                get_template_part('templates/tr-material');
+              endwhile;
+            endif;
+            wp_reset_postdata(); ?>
+          </div><!-- tr material category -->
+
+          <div class="column">
+            <h2 class="heading--tertiary mg-bottom--medium">Horticulture</h2>
+            <?php $args = array(
+              'post_type'         => 'training_material',
+              'posts_per_page'    => '-1',
+              'tax_query'         => array(
+                array(
+                  'taxonomy'      => 'training_material_category',
+                  'field'         => 'term_id',
+                  'terms'         => '15'
+                )
+              )
+            );
+            $resources = new WP_Query($args);
+
+            if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+                get_template_part('templates/tr-material');
+              endwhile;
+            endif;
+            wp_reset_postdata(); ?>
+          </div><!-- tr material category -->
+
+          <div class="column">
+            <h2 class="heading--tertiary mg-bottom--medium">Crosscutting</h2>
+            <?php $args = array(
+              'post_type'         => 'training_material',
+              'posts_per_page'    => '-1',
+              'tax_query'         => array(
+                array(
+                  'taxonomy'      => 'training_material_category',
+                  'field'         => 'term_id',
+                  'terms'         => '18'
+                )
+              )
+            );
+            $resources = new WP_Query($args);
+            if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+                get_template_part('templates/tr-material');
+              endwhile;
+            endif;
+            wp_reset_postdata(); ?>
+          </div><!-- tr material category -->
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- E-course -->
+<section class="section--padded pd-top--big pd-bottom--big">
+  <div class="container">
+    <h2 class="heading--secondary heading--green mg-bottom--big">E-Courses</h2>
+    <div class="columns is-multiline">
+      <?php $args = array(
+        'post_type'         => 'ecourse',
+        'order' => 'desc',
+        'orderby' => 'publish_date',
+        'posts_per_page' => '-1'
+      );
+      $resources = new WP_Query($args);
+
+      if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+          get_template_part('templates/e-course');
+        endwhile;
+      endif;
+      wp_reset_postdata();
+      ?>
+    </div>
+  </div>
+</section>
+<!-- NewsLetter -->
+<section class="section pd-top--big pd-bottom--big bg-light-blue">
+  <div class="container">
+    <h2 class="heading--secondary heading--green mg-bottom--medium">
+      Newsletter
+    </h2>
+    <div class="columns is-multiline">
+      <?php $args = array(
+        'order'    => 'desc',
+        'orderby'  => 'publish_date',
+        'posts_per_page' => '-1',
+        'cat' => '10'
+      );
+      $resources = new WP_Query($args);
+
+      if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+          get_template_part('templates/newsletter');
+        endwhile;
+      endif;
+      wp_reset_postdata();
+      ?>
+    </div>
+  </div>
+</section>
+<!-- newsletter -->
+<section class="section--padded pd-top--big pd-bottom--big">
+  <div class="container">
+    <h2 class="heading--secondary heading--green mg-bottom--big">Gallery</h2>
+    <div class="columns is-multiline">
+      <?php $args = array(
+        'order'    => 'desc',
+        'orderby'  => 'publish_date',
+        'posts_per_page' => '-1',
+        'cat' => '8'
+      );
+      $resources = new WP_Query($args);
+
+      if ($resources->have_posts()) : while ($resources->have_posts()) : $resources->the_post();
+          get_template_part('templates/resource');
+        endwhile;
+      endif;
+      wp_reset_postdata();
+      ?>
+    </div>
+  </div>
+</section>
+<!-- gallery -->
+<!-- gallery -->
+<?php get_footer(); ?>
