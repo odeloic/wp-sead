7b7431cb03a33796c229a9da5c7d76cf326153ff
Ode Manzi <manzii.ode@gmail.com>
2020-05-18 23:20:49 +0800

    added id on page-titles

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,4 @@
-<div class="page-header">
-
+<div class="page-header" id="<?php echo strtolower(str_replace(' ', '-', get_the_title())); ?>">
   <?php if (get_post_thumbnail_id()) : ?>
     <img src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title(); ?>" class="page-header__cover">
   <?php endif; ?>
