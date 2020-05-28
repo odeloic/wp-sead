2319459ddb5d9d452a155435c69db4936f7bc0ff
Ode Manzi <manzii.ode@gmail.com>
2020-05-28 13:15:34 +0800

    bumped duration of testimonials slider

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -47,7 +47,7 @@ var testimonialsSlider = new Swiper(".testimonials", {
     spaceBetween: 30,
     loop: true,
     autoplay: {
-        delay: 5000,
+        delay: 10000,
     },
     pagination: {
         el: ".swiper-pagination",
