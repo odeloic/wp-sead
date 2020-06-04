cc7d9274fc39ba44dd7ccc78114393155662a749
Ode Manzi <manzii.ode@gmail.com>
2020-06-04 16:54:01 +0800

    restore google analytics tracking code

--- header.php
+++ header.php
@@ -5,6 +5,19 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152899487-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-152899487-1');
+    </script>
+
     <?php wp_head(); ?>
 </head>
 
